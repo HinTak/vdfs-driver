d7bc7db63995af7fa180daa5435db1fcc872b6f7
Hin-Tak Leung <htl10@users.sourceforge.net>
2020-05-01 08:53:52 +0100

    current_thread_info() changed entirely, current cpu moved to task_struct in v4.9
    commit 15f4eae70d365bba26854c90b6002aaabb18c8aa
    Author: Andy Lutomirski <luto@kernel.org>
    Date:   Tue Sep 13 14:29:25 2016 -0700
    
        x86: Move thread_info into task_struct
    
        Now that most of the thread_info users have been cleaned up,
        this is straightforward.
    
        Most of this code was written by Linus.
    
    commit c65eacbe290b8141554c71b2c94489e73ade8c8d
    Author: Andy Lutomirski <luto@kernel.org>
    Date:   Tue Sep 13 14:29:24 2016 -0700
    
        sched/core: Allow putting thread_info into task_struct
    
        If an arch opts in by setting CONFIG_THREAD_INFO_IN_TASK_STRUCT,
        then thread_info is defined as a single 'u32 flags' and is the first
        entry of task_struct.  thread_info::task is removed (it serves no
        purpose if thread_info is embedded in task_struct), and
        thread_info::cpu gets its own slot in task_struct.
    
        This is heavily based on a patch written by Linus.

--- decompress.c
+++ decompress.c
@@ -78,7 +78,7 @@ static void print_uncomp_err_dump_header(char *name, int name_len)
 			task_pid_nr(current));
 	VDFS4_ERR("--------------------------------------------------");
 	VDFS4_ERR("== VDFS4 Debugger - %15s ===== Core : %2d ===="
-			, VDFS4_VERSION, current_thread_info()->cpu);
+			, VDFS4_VERSION, current->cpu);
 	VDFS4_ERR("--------------------------------------------------");
 	VDFS4_ERR("Source image name : %.*s", name_len, name);
 	VDFS4_ERR("--------------------------------------------------");
